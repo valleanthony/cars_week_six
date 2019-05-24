49655f29e28d057a75282e9cf5de0b26f4cc9b66
Anthony Valle <valleganthony@gmail.com>
2019-05-24 09:08:42 -0400

    Initial commit

---
+++ src/main/java/com/example/cars_week_six/CarsWeekSixApplication.java
@@ -0,0 +1,13 @@
+package com.example.cars_week_six;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsWeekSixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarsWeekSixApplication.class, args);
+	}
+
+}
